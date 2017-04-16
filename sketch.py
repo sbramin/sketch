bd0dbe90384493f336be0c4796f3f06319d3a04b
sbr <sascha.ramin@sky.uk>
2017-04-16 15:22:38 +0100

    pad class working, removed help function as I don't want to have to keep state of drawing and redraw the pad every time I clear the screen

--- sketch.py
+++ sketch.py
@@ -53,41 +53,18 @@ def logging_level(level, name):
         getLogger("paramiko").setLevel(DEBUG)
 
 
-def sketch_setup():
-    stdscr = curses.initscr()
-    curses.noecho()
-    curses.cbreak()
-    curses.curs_set(0)
-
-    if curses.has_colors():
-        curses.start_color()
-
-    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
+def sketch_setup(stdscr):
 
     stdscr.addstr("Sketch Pad", curses.A_REVERSE)
     stdscr.chgat(-1, curses.A_REVERSE)
-    stdscr.addstr(curses.LINES-1, 0, "Enter Q to quit")
-    stdscr.chgat(curses.LINES-1, 7, 1, curses.A_BOLD | curses.color_pair(2))
+    stdscr.addstr(curses.LINES - 1, 0, "Enter Q to quit")
+    stdscr.chgat(curses.LINES - 1, 7, 1, curses.A_BOLD | curses.color_pair(2))
 
-    outer_frame = curses.newwin(curses.LINES-2, curses.COLS, 1,0)
-
-    command_frame = outer_frame.subwin(curses.LINES-6, curses.COLS-4, 3, 2)
-
+    outer_frame = curses.newwin(curses.LINES - 2, curses.COLS, 1, 0)
     outer_frame.box()
     stdscr.noutrefresh()
     outer_frame.noutrefresh()
     curses.doupdate()
-
-    return command_frame
-
-
-def sketch_teardown():
-    curses.nocbreak()
-    curses.echo()
-    curses.curs_set(1)
-    curses.endwin()
 
 
 def sketch_print(frame, text):
@@ -100,7 +77,7 @@ def sketch_print(frame, text):
 
 def sketch_error(frame, prompt, err=""):
     if err == "":
-        err = "Invalid input please enter h if you need some help"
+        err = "Invalid input please read README.md if you need help"
         sketch_print(frame, "{}{}.".format(prompt, err))
     else:
         sketch_print(frame, "{}Error: {}.".format(prompt, err))
@@ -108,67 +85,82 @@ def sketch_error(frame, prompt, err=""):
     sketch_print(frame, prompt)
 
 
-def create_pad(command_frame, w, h):
-    max_w = curses.COLS-8
-    max_h = curses.LINES-8
-    if w > max_w or h > max_h or w < 2 or h < 2:
-        return None, "Illegal pad size, pad must be between w:2-{} h:2-{}".format(max_w, max_h)
-    pad = command_frame.derwin(h, w, 2, 2)
-    pad.box()
-    pad.noutrefresh()
-    curses.doupdate()
-    return pad, None
+class Pad(object):
+    def __init__(self, w=0, h=0):
+        self.w = w
+        self.h = h
+        self.max_w = curses.COLS - 4
+        self.max_h = curses.LINES - 6
+
+    def new(self):
+        if self.w > self.max_w or self.h > self.max_h or self.w < 2 or self.h < 2:
+            return "Illegal pad size, pad must be between w:2-{} h:2-{}".format(
+                self.max_w, self.max_h)
+        self.frame = curses.newwin(self.h, self.w, 4, 2)
+        self.frame.box()
+        self.frame.noutrefresh()
+        curses.doupdate()
+        return None
+
+    def used(self):
+        if self.w == 0 or self.h == 0:
+            return False
+        else:
+            return True
+
+    def frame(self):
+        return self.frame
 
 
-def sketch_input(command_frame):
+def sketch_input():
     prompt = "Enter command: "
+    input_frame = curses.newwin(1, curses.COLS - 4, 3, 2)
     curses.echo()
-    sketch_print(command_frame, prompt)
+    sketch_print(input_frame, prompt)
+    pad = Pad()
     while True:
-        c = command_frame.getstr()
+        c = input_frame.getstr()
         debug(c)
         if len(c) > 1:
             cmd = c.decode("utf8").split()
             debug(cmd)
             if cmd[0].lower() == 'c' and len(cmd) == 3:
-                try:
-                    w = int(cmd[1])
-                    h = int(cmd[2])
-                    sketch_print(command_frame, prompt)
-                    pad, err = create_pad(command_frame, w, h)
-                    if err != None:
-                        sketch_error(command_frame, prompt, err)
-                except Exception as err:
-                    sketch_error(command_frame, prompt, str(err))
+                if pad.used():
+                    sketch_error(
+                        input_frame, prompt,
+                        "Pad already created, get drawing or quit to start again"
+                    )
+                else:
+                    try:
+                        w = int(cmd[1])
+                        h = int(cmd[2])
+                        sketch_print(input_frame, prompt)
+                        pad = Pad(w, h)
+                        err = pad.new()
+                        if err != None:
+                            sketch_error(input_frame, prompt, err)
+                    except Exception as err:
+                        sketch_error(input_frame, prompt, str(err))
             elif cmd[0].lower() == 'l' and len(cmd) == 5:
                 pass
             else:
-                sketch_error(command_frame, prompt)
+                sketch_error(input_frame, prompt)
         elif c == b'':
-            sketch_print(command_frame, prompt)
+            sketch_print(input_frame, prompt)
         elif c == b'q' or c == b'Q':
             break
-        elif c == b'h' or c == b'H':
-            help_text = """
-            Some text
-            Potentially lots
-            on several lines
-            """
-            sketch_print(command_frame, help_text)
         else:
-            sketch_error(command_frame, prompt)
+            sketch_error(input_frame, prompt)
 
         curses.doupdate()
     curses.noecho()
 
 
-def main():
+def main(stdscr):
     logging_level('debug', 'sketch')
-    main_frame = sketch_setup()
-    sketch_input(main_frame)
-    sketch_teardown()
+    sketch_setup(stdscr)
+    sketch_input()
 
 
 if __name__ == "__main__":
-    main()
-
+    curses.wrapper(main)
